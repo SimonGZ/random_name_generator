50e935295efa01e76f51a36f82ace420ac7dbfd4
Simon Ganz <simong@gmail.com>
2012-09-30 11:22:24 -0700

    Making a quick commit before implementing some bigger changes

--- test/functional/random_controller_test.rb
+++ test/functional/random_controller_test.rb
@@ -4,21 +4,32 @@ class RandomControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_template :home
   end
 
   test "should get first" do
     get :first
     assert_response :success
+    assert_template :first
   end
 
   test "should get last" do
     get :last
     assert_response :success
+    assert_template :last
   end
 
   test "should get full" do
     get :full
     assert_response :success
+    assert_template :full
+  end
+
+  test "should get correct names" do
+    params_hash = {:nametype => 'full', :gender => 'Female'}
+    get :generate, params_hash
+    assert_response :success
+    assert_template :full, "Template should match nametype"
   end
 
 end
--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -5,12 +5,16 @@ class RoutesTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   
+  test "root leads to random#home" do
+    assert_recognizes({:controller => 'random', :action => 'home'}, '', {}, 'Path root should lead to random#home')
+  end
+  
   test "generate route" do
     assert_recognizes({:controller => 'random', :action => 'generate'}, 'generate', {}, 'Path generate should lead to random#generate')
   end
   
-  test "root leads to random#home" do
-    assert_recognizes({:controller => 'random', :action => 'home'}, '', {}, 'Path root should lead to random#home')
+  test "generate GET produces proper params" do
+    assert_recognizes({:controller => 'random', :action => 'generate', :limit => '10', :gender => 'Female', :nametype => 'full'}, 'generate', { :limit => '10', :gender => 'Female', :nametype => 'full' }, 'Generate GET should return correct options')
   end
   
 end
